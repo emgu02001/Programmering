dc558ad243064d9c5536318227c38da788ee8c8d
emgu02001 <70271390+emgu02001@users.noreply.github.com>
2020-11-16 15:35:59 +0100

    Add files via upload
    Slumptal

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int random1 = slump.Next(0, 101);
+            int gissningar = 0;
+
+            Console.Write("Skriv ett tal mellan 1 - 100: ");
+            int gissning = int.Parse(Console.ReadLine());
+            gissningar++;
+
+            while (gissning != random1)
+            {
+                if( gissning < random1)
+                {
+                    Console.WriteLine("För lågt, gissa högre!" + gissning );
+                    gissningar++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Du gissade för högt, gissa lägre!" + gissning);
+                    gissningar++;
+                }
+               
+                gissning = int.Parse(Console.ReadLine());
+               
+            }
+            Console.WriteLine("Du gissade rätt!");
+                Console.WriteLine("du gissade såhär många gånger: " + gissningar);
+
+
+
+
+
+        }
+    }
+}
